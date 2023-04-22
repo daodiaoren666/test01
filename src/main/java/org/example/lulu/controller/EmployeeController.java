dcb4c8e4684e0cedb2b7fbdc46666759135abd9c
lulu <2417471135@qq.com>
2023-04-22 21:10:12 +0800

    init:初始化项目

--- src/main/java/org/example/lulu/controller/EmployeeController.java
+++ src/main/java/org/example/lulu/controller/EmployeeController.java
@@ -139,8 +139,10 @@ private AutionUtis autionUtis;
         if(management==null) {
            return R.error("信息填写错误");
         }else {
+            int let=6;
             employeeService.save(management);
             return R.success("添加管理员成功");
+
         }
     }
 
